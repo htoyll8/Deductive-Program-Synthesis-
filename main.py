1f7852314ac02f87be90ee4b1a3a6cd27495eef4
Rita Holloway <ritaholloway@dhcp-10-250-100-183.harvard.edu>
2024-03-08 17:14:31 -0500

    Added subtraction by zero rule.

--- main.py
+++ main.py
@@ -18,6 +18,16 @@ class EliminateAdditionWithZero:
             return node.left
         else: 
             return node.right
+        
+class EliminateSubtractionByZero: 
+    
+    @classmethod
+    def check(cls, node: ast.BinOp) -> bool:
+        return isinstance(node, ast.BinOp) and isinstance(node.op, ast.Sub) and isinstance(node.right, ast.Constant) and node.right.value == 0 
+    
+    @classmethod
+    def apply(self, node: ast.BinOp) -> ast.AST:
+        return node.left
 
 class EliminateMultiplicationByOne:
 
@@ -42,6 +52,8 @@ class Visitor(ast.NodeTransformer):
     def visit_BinOp(self, node: ast.AST) -> ast.AST:
         if EliminateAdditionWithZero.check(node):
             return EliminateAdditionWithZero.apply(node)
+        elif EliminateSubtractionByZero.check(node):
+            return EliminateSubtractionByZero.apply(node)
         elif EliminateMultiplicationByOne.check(node):
             return EliminateMultiplicationByOne.apply(node)
         else:
@@ -71,6 +83,27 @@ class TestRewriteStrategies(unittest.TestCase):
         simplified_ast = visitor.visit(addition_node)
         self.assertEqual(simplified_ast, right)
 
+    def test_eliminate_subtraction_by_zero(self):
+        # Create an instance of Visitor
+        visitor = Visitor()
+
+        # Create an AST node representing 'x - 0'
+        left = ast.Name(id='x', ctx=ast.Load()) 
+        right = ast.Constant(0)
+        subtraction_node = ast.BinOp(left, ast.Sub(), right)
+
+        # Apply the strategy and check the result
+        simplified_ast = visitor.visit(subtraction_node)
+        self.assertEqual(simplified_ast, left)
+
+        # Create an AST node representing '0 - x'
+        left = ast.Constant(0)
+        right = ast.Name(id='x', ctx=ast.Load()) 
+        subtraction_node = ast.BinOp(left, ast.Sub(), right)
+
+        # Apply the strategy and check the result
+        simplified_ast = visitor.visit(subtraction_node)
+        self.assertEqual(simplified_ast, subtraction_node)
 
     def test_eliminate_multiplication_by_one(self):
         # Create an instance of Visitor
